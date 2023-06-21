37138350cd4f55cab1cdbd0afe18391d84727bef
David C Ellis <ducksual@gmail.com>
2023-06-21 21:55:45 +0100

    Attempt to prevent windows sounds from playing on notifications.

--- src/steam_actionset_display/notifier/toast.py
+++ src/steam_actionset_display/notifier/toast.py
@@ -3,6 +3,8 @@ from windows_toasts import (
     ToastImageAndText2,
     ToastDisplayImage,
     ToastDuration,
+    ToastAudio,
+    AudioSource,
 )
 
 from .base import BaseNotifier
@@ -17,12 +19,21 @@ class ToastNotifier(BaseNotifier):
         self.toaster = WindowsToaster(self.settings["title"])
 
         self.image = ToastDisplayImage.fromPath(icon_file, circleCrop=False)
+        # Sound or No Sound
+        audio = ToastAudio(
+            sound=AudioSource.Default,
+            looping=False,
+            silent=self.settings["mute"]
+        )
 
         self.toast = ToastImageAndText2()
+
+        self.toast.SetAudio(audio)
         self.toast.SetSuppressPopup(False)
         self.toast.SetDuration(ToastDuration.Short)
         self.toast.AddImage(self.image)
         self.toast.SetHeadline(self.settings["header"])
+
 
     def display(self, message):
         self.toast.SetFirstLine(message)
