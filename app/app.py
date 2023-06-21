d651fea6487b72f449ddde9ba22f16f0379413d3
David C Ellis <ducksual@gmail.com>
2023-06-21 16:17:47 +0100

    Add a catch-all exception catch for the application to prevent the window from immediately closing.

--- app/app.py
+++ app/app.py
@@ -10,4 +10,8 @@ else:
 
 config_path = base_path / "config.toml"
 
-run_application(config_path)
+try:
+    run_application(config_path)
+except Exception as e:
+    print(f"An exception occured and the program is closing: {type(e).__name__} - {e}")
+    input("Press Enter to close...")
